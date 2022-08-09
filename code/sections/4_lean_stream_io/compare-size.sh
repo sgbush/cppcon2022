f6567e7ec9c44bc20581137f5c07202f7ecb2a99
Steve Bush <bush.sg@pg.com>
2022-08-09 18:25:23 -0400

    Improve compilation script to search more paths

--- code/sections/4_lean_stream_io/compare-size.sh
+++ code/sections/4_lean_stream_io/compare-size.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+CXX=$(find /usr/bin /opt/arm -name arm-none-eabi-g++ -print -quit)
+[[ -z $CXX ]] && { echo "Compiler not found!"; exit -1; }
 
-CXX=$(find /usr/bin -name arm-none-eabi-g++)
+$CXX --version
+
 CXXFLAGS="-g0 -Os -std=c++20 \
 -march=armv7e-m+fp -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mtune=cortex-m4 \
 -Wall -Wextra -Wpedantic -Wno-psabi \
 -fno-rtti -fno-exceptions -ffunction-sections \
 --specs=nosys.specs -Wl,--gc-sections"
 
-OBJCOPY=$(find /usr/bin -name arm-none-eabi-objcopy)
+OBJCOPY=$(find /usr/bin /opt/arm -name arm-none-eabi-objcopy)
+[[ -z $OBJCOPY ]] && { echo "Object copy not found!"; exit -1; }
 
 
 $CXX $CXXFLAGS main-conventional.cpp -o main-conventional.elf
